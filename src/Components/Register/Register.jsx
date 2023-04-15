3224b3eba1de000f76f1e3c3fd2b81c3c991ba90
ariaan ahmed <you@example.com>
2023-04-15 15:45:06 +0600

    59-5 Login form field validation using regular expression

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,36 +1,56 @@
-import React from 'react';
-import './Register.css'
+import React, { useState } from 'react';
+import './Register.css';
 
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import app from '../../firebase/firebase.config';
-import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth';
+
 const auth = getAuth(app)
 
 const Register = () => {
-
-    const handleOnChange = (event) =>{
-        event.preventDefault()
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('')
+    const handleOnChange = (event) => {
+        event.preventDefault();
+        setSuccess('')
+        setError('')
         const email = event.target.email.value;
         const password = event.target.password.value;
         console.log(email, password)
 
-        createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
-            const loggedInUser = userCredential.user;
-            console.log(loggedInUser)
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setError('please add atleast one uppercase');
+            return;
+        } else if (!/(?=.*[0-9].*[0-9])/.test(password)) {
+            setError('please add at least two numbers')
+            return;
+        } else if (password.length < 6) {
+            setError('please add at least 6 characters in your password')
+            return;
+        }
+        createUserWithEmailAndPassword(auth, email, password).then((result) => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            setError('');
+            event.target.reset();
+            setSuccess('user has been created')
+
+
         }).catch((error) => {
-            console.error(error);
+            console.error(error.message)
+            setError(error.message)
         })
     }
-
-
     return (
         <div>
             <h4 className='regHeading'>please register</h4>
             <form onSubmit={handleOnChange} className='register-container'>
-                <input type="email" name="email" id="email" placeholder='Enter you email' />
+                <input type="email" required name="email" id="email" placeholder='Enter you email' />
                 <br />
-                <input type="password" name="password" id="password" placeholder='Password' /> <br />
+                <input type="password" required name="password" id="password" placeholder='Password' /> <br />
                 <button type='submit'>Register</button>
             </form>
+            <p className='text-center'>{error}</p>
+            <p className='text-center'>{success}</p>
         </div>
     );
 };
